299482c36af6268b0dc764428d7c5b0f0344a3b5
kyohei7 <rizkimuhammad2301@gmail.com>
2025-05-26 10:14:48 +0700

    logging challenge

--- main.go
+++ main.go
@@ -8,7 +8,9 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
 
@@ -28,6 +30,22 @@ func main() {
 	r.HandleFunc("/", handler)
 
 	// start: set up any of your logger configuration here if necessary
+	// set global log level to debug
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	// create directory logs if not exist
+	if _, err := os.Stat("logs"); os.IsNotExist(err) {
+		os.Mkdir("logs", 0755)
+	}
+	// open file for log
+	file, err := os.OpenFile("logs/app.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed To Open Log File")
+	}
+	// configuration multiple writer for console and file
+	consoleWriter := zerolog.ConsoleWriter{Out: os.Stdout}
+	multiWriter := zerolog.MultiLevelWriter(consoleWriter, file)
+	// set output global logger
+	log.Logger = zerolog.New(multiWriter).With().Timestamp().Logger()
 
 	// end: set up any of your logger configuration here
 
@@ -46,22 +64,61 @@ func main() {
 	if err := server.Shutdown(context.Background()); err != nil {
 		log.Error().Err(err).Msg("failed to shutdown http server gracefully")
 	}
+
+	// Simple Logging Example
+	log.Info().Msg("Welcome To Instrumentation Class")
+	log.Debug().Msg("This is a debug log at application shutdown.")
+	log.Error().Msg("This is an error log during application shutdown. (Simulated)")
 }
 
+type requestIDCtxKey string
+
+const requestIdKey requestIDCtxKey = "request_id"
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
+	// generate new request_id
+	reqID := uuid.New().String()
+	// add request_id to context
+	ctx := context.WithValue(r.Context(), requestIdKey, reqID)
+
+	// create logger with request_id and function name
+	log := log.With().Str(string(requestIdKey), reqID).Str("func", "handler").Logger()
+	log.Debug().Msg("Incoming request received.")
+
 	name := r.URL.Query().Get("name")
 	res, err := greeting(ctx, name)
 	if err != nil {
+		log.Error().Err(err).Msg("Error in greeting function")
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	log.Info().Msgf("Successfully processed request for name : %s", name)
 	w.Write([]byte(res))
+
+	// ctx := r.Context()
+	// name := r.URL.Query().Get("name")
+	// res, err := greeting(ctx, name)
+	// if err != nil {
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
+	// w.Write([]byte(res))
 }
 
 func greeting(ctx context.Context, name string) (string, error) {
+	reqID, ok := ctx.Value(requestIdKey).(string)
+	if !ok {
+		reqID = "unknown" // fallback if not found
+	}
+
+	// create logger with request_id and function name
+	log := log.With().Str(string(requestIdKey), reqID).Str("func", "greeting").Logger()
+	log.Debug().Msgf("Checking name length for: %s", name)
+
 	if len(name) < 5 {
+		log.Warn().Msg("Name is to short.")
 		return fmt.Sprintf("Hello %s! Your name is to short\n", name), nil
 	}
+	log.Info().Msgf("Name length is sufficient for: %s", name)
 	return fmt.Sprintf("Hi %s", name), nil
 }
